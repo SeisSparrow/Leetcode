fedfd5d9922a3f8c5ec96f314b7ef62e98be4d8b
SeisSparrow <zhennan2010@hotmail.com>
2017-05-06 00:14:11 -0500

    Add files via upload

--- python/344.py
+++ python/344.py
@@ -1,1 +1,20 @@
+class Solution(object):
 
+    def reverseString(self, s):
+
+        """
+
+        :type s: str
+
+        :rtype: str
+
+        """
+        letters = list(s)
+        letters_reverse = []
+        for i in range (0, len(letters)):
+            letters_reverse.append(letters[len(letters)-1-i])
+        s_reverse = ''.join(letters_reverse)    
+        return s_reverse
+
+a = Solution()
+print a.reverseString("shit")
