54f1163acf06326a80c48d97e49df038aa9a77e8
SeisSparrow <zhennan2010@hotmail.com>
2017-05-08 00:16:35 -0500

    Add files via upload

---
+++ python/461.py
@@ -0,0 +1,24 @@
+class Solution(object):
+
+    def hammingDistance(self, x, y):
+
+        """
+
+        :type x: int
+
+        :type y: int
+
+        :rtype: int
+
+        """
+        z = x ^ y
+        z_list = list(bin(z))
+        N = len(z_list)
+        num = 0
+        for i in range(2,N):
+            if int(z_list[i]) != 0:
+		num += 1
+        return num
+
+#a=Solution()
+#print a.hammingDistance(9,4)
