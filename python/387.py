8cd08dafdf17c8c3dbc2fe639e97d8dd7485f014
SeisSparrow <zhennan2010@hotmail.com>
2017-05-17 15:25:59 -0500

    Add files via upload

--- python/387.py
+++ python/387.py
@@ -1,3 +1,5 @@
+import operator
+
 class Solution(object):
 
     def firstUniqChar(self, s):
@@ -12,31 +14,46 @@ class Solution(object):
 
         letters = list(s)
         N = len(letters)
-        dic = {}
-        for i in range(0, N):
-            dic.update({i: letters[i]})
-        location = -1
-        letters_sorted = sorted(letters)
         
         if N >= 2: 
+
+	    location = -1
+            dic = {}
+
             for i in range(0, N):
-                if i == 0:
-                    if letters_sorted[i] != letters_sorted[i+1]:
-                        location = dic.keys()[dic.values().index(letters_sorted[i])]
-                elif i > 0 and i <= N-2:
-                    if letters_sorted[i-1] != letters_sorted[i] and letters_sorted[i] != letters_sorted[i+1]:
-		        if dic.keys()[dic.values().index(letters_sorted[i])] < location:
-			    location = dic.keys()[dic.values().index(letters_sorted[i])]
-                else:
-                    if letters_sorted[i-1] != letters_sorted[i]:
-                        if dic.keys()[dic.values().index(letters_sorted[i])] < location:
-                            location = dic.keys()[dic.values().index(letters_sorted[i])]
-	else:
-            return 0
+            	dic.update({i: letters[i]})
+
+	    dic_sorted = sorted(dic.items(), key=operator.itemgetter(1))
+	    
+	    for i in range(0, N):
+		if i == 0 and dic_sorted[i][1] != dic_sorted[i+1][1]:
+                    if location != -1 and location > dic_sorted[i][0]:
+                        location = dic_sorted[i][0]
+                    if location == -1:
+                        location = dic_sorted[i][0]
+		
+		if i >= 1 and i <= N-2 and dic_sorted[i][1] != dic_sorted[i+1][1] and dic_sorted[i-1][1] != dic_sorted[i][1]:
+		    if location != -1 and location > dic_sorted[i][0]:
+                        location = dic_sorted[i][0]
+		    if location == -1:
+                        location = dic_sorted[i][0]
+
+		if i == N-1 and dic_sorted[i-1][1] != dic_sorted[i][1]:
+
+                    if location != -1 and location > dic_sorted[i][0]:
+                        location = dic_sorted[i][0]
+                    if location == -1:
+                        location = dic_sorted[i][0]
+	
+	if N == 1:
+            location = 0
+
+	if N == 0:
+	    location = -1
 	    
 	return location
 
 a = Solution()
-sss = "loveleetcode"
+sss = "a"
 print a.firstUniqChar(sss)    
         
